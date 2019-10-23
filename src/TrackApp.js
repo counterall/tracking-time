4046f2a4cd13ac5fa999f97f2c70d1a672de5a30
Kan Cong <kan.cong@digia.com>
2019-10-23 14:40:35 +0300

    create parent time tracking app js

---
+++ src/TrackApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TrackApp extends Component {
+    constructor(props){
+        super();
+    }
+
+    render() {
+        return <div className="wrapper"></div>
+    }
+}
+
+
+export default TrackApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
-import App from './App';
+import TrackApp from './TrackApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TrackApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
