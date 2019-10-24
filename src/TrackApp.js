8513424b0d8a4e91a15f6ce241210543d72b5657
Kan Cong <kan.cong@digia.com>
2019-10-24 17:38:14 +0300

    Initialise localStorage

--- src/TrackApp.js
+++ src/TrackApp.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import './style/trackapp.scss';
 import ActiveTask from "./components/ActiveTask";
+import Crud from "./helpers/crud";
+
+const basicStructure = {
+    task_count: 0
+}
+
+if (localStorage.getItem("task_number") === null) {
+    Object.keys(basicStructure).forEach((k) => {
+        localStorage.setItem(k, basicStructure[k])
+    });
+}
 
 class TrackApp extends Component {
     constructor(props){
