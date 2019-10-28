5edc568fc23d2bca0dd7c69d87b453869282e75f
Kan Cong <kcongmj23@gmail.com>
2019-10-28 20:00:48 +0200

    code refactoring to put timer logic into trackapp root component

--- src/TrackApp.js
+++ src/TrackApp.js
@@ -19,27 +19,89 @@ if (localStorage.getItem("task_count") === null) {
 class TrackApp extends Component {
     constructor(props){
         super(props);
+        Crud.removeActiveTask();
+
+        const {activeTaskTime, activeTaskTag, activeTaskName} = {...Crud.getActiveTask()};
+
         this.state = {
-            activeTask: Crud.getActiveTask()
+            activeTaskTS: typeof activeTaskTime !== "undefined" ? activeTaskTime : 0,
+            activeTaskTag: typeof activeTaskTag !== "undefined" ? activeTaskTag : "",
+            activeTaskName: typeof activeTaskName !== "undefined" ? activeTaskName : "",
+            activeTaskIsRunning: false
         };
+
         this.handleAddTask = this.handleAddTask.bind(this);
+        this.handleFinishClick = this.handleFinishClick.bind(this);
+        this.handleResumeClick = this.handleResumeClick.bind(this);
+
     }
 
     handleAddTask(newTask) {
         if (Object.keys(newTask).length) {
+            const newState = {
+                activeTaskTag: newTask.tag,
+                activeTaskName: newTask.name,
+                activeTaskTS: 0,
+                activeTaskIsRunning: true
+            };
+            this.setState(() => {
+                return newState
+            }, this.initialiseDisplay);
+        }
+    }
+
+    handleResumeClick() {
+        this.setState((state) => {
+            return {activeTaskIsRunning: !state.activeTaskIsRunning}
+        }, this.initialiseDisplay);
+    }
+
+    handleFinishClick() {
+
+    }
+
+    // if active task is still running when page is refreshed, save data of active task to localStorage
+    saveActiveTaskDurationBeforePageReload() {
+        window.onunload = () => {
+            if (this.state.activeTaskTS > 0) {
+                const oldActiveTask = Crud.getActiveTask();
+                const newActiveTask = {...oldActiveTask, duration: this.state.activeTaskTS};
+                Crud.setActiveTask(newActiveTask);
+            }
+        };
+    }
+
+    componentDidMount() {
+        this.initialiseDisplay();
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    initialiseDisplay() {
+        clearInterval(this.timer);
+        let originalTmstp = Math.floor(Date.now() / 1000);
+        originalTmstp = originalTmstp - this.state.activeTaskTS;
+
+        if (this.state.activeTaskIsRunning) {
+            this.timer = setInterval(() => {
+                this.setState({
+                    activeTaskTS: Math.floor(Date.now() / 1000) - originalTmstp
+                });
+            }, 1000);
+        } else {
             this.setState({
-                activeTask: {
-                    meta: newTask,
-                    duration: 0
-                }
+                activeTaskTS: Math.floor(Date.now() / 1000) - originalTmstp
             });
         }
+
     }
 
     render() {
 
         return <div className="wrapper">
-            <ActiveTask {...this.state.activeTask} />
+            <ActiveTask {...this.state} handleFinishClick={this.handleFinishClick} handleResumeClick={this.handleResumeClick} />
             <AddNewTask handleAddTask={this.handleAddTask} />
         </div>
     }
--- src/components/ActiveTask.js
+++ src/components/ActiveTask.js
@@ -2,102 +2,24 @@ import React, { Component } from 'react';
 import TimeDisplay from "./active_task/TimeDisplay";
 import TaskInfo from "./active_task/TaskInfo";
 import Buttons from "./active_task/Buttons";
-import Crud from "../helpers/crud";
 import "../style/modules/_active_task.scss";
 
 class ActiveTask extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            timestamp: 0,
-            isRunning: false
-        };
-        this.handleResumeClick = this.handleResumeClick.bind(this);
-        // this.saveActiveTaskDurationBeforePageReload();
-        Crud.removeActiveTask();
-    }
-
-    handleResumeClick() {
-        this.setState((state) => {
-            return {isRunning: !state.isRunning}
-        }, this.initialiseDisplay);
-    }
-
-    handleFinishClick() {
-
-    }
-
-    // if active task is still running when page is refreshed, save data of active task to localStorage
-    saveActiveTaskDurationBeforePageReload() {
-        window.onunload = () => {
-            if (this.state.timestamp > 0) {
-                const oldActiveTask = Crud.getActiveTask();
-                const newActiveTask = {...oldActiveTask, duration: this.state.timestamp};
-                Crud.setActiveTask(newActiveTask);
-            }
-        };
-    }
-
-    componentDidMount() {
-        this.initialiseDisplay();
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
-    }
-
-    initialiseDisplay() {
-        clearInterval(this.timer);
-        let originalTmstp = Math.floor(Date.now() / 1000);
-
-        // pause or resume the currently active task
-        if (this.state.timestamp) {
-            originalTmstp = originalTmstp - this.state.timestamp;
-            if (this.state.isRunning) {
-                this.timer = setInterval(() => {
-                    this.setState({
-                        timestamp: Math.floor(Date.now() / 1000) - originalTmstp
-                    });
-                }, 1000);
-            } else {
-                this.setState({
-                    timestamp: Math.floor(Date.now() / 1000) - originalTmstp
-                });
-            }
-        }
-        // no currently active task found
-        else {
-            if (typeof this.props.duration !== 'undefined') {
-                originalTmstp = originalTmstp - this.props.duration;
-                if (this.props.duration === 0) {
-                    // automatically start ticking if duration of active task is 0, .i.e creating a new active task.
-                    this.setState({
-                        isRunning: true
-                    });
-                    this.timer = setInterval(() => {
-                        this.setState({
-                            timestamp: Math.floor(Date.now() / 1000) - originalTmstp
-                        });
-                    }, 1000);
-                } else {
-                    this.setState({
-                        timestamp: Math.floor(Date.now() / 1000) - originalTmstp
-                    });
-                }
-            }
-        }
-    }
 
     render() {
         let content;
         let cls = ['active-task'];
-        let activeTaskTime = this.state.timestamp > 0 ? this.state.timestamp : this.props.duration;
-        if (activeTaskTime >= 0) {
+
+        if (this.props.activeTaskTag) {
+            const taskMeta = {
+                tag: this.props.activeTaskTag,
+                name: this.props.activeTaskName
+            }
             content = (
                 <React.Fragment>
-                    <TimeDisplay timestamp={activeTaskTime} />
-                    <TaskInfo meta={this.props.meta} />
-                    <Buttons taskIsRunning={this.state.isRunning} resumeTask={this.handleResumeClick} finishTask={()=>{this.handleFinishClick()}} />
+                    <TimeDisplay timestamp={this.props.activeTaskTS} />
+                    <TaskInfo meta={taskMeta} />
+                    <Buttons taskIsRunning={this.props.activeTaskIsRunning} resumeTask={this.props.handleResumeClick} finishTask={this.props.handleFinishClick} />
                 </React.Fragment>
             );
         } else {
