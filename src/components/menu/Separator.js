64377ee50d1edc1c75a747904f10888a382812f1
Kan Cong <kan.cong@digia.com>
2019-11-19 15:21:06 +0200

    Add separator component to separate blocks on menu page

---
+++ src/components/menu/Separator.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../style/modules/menu/_separator.scss";
+
+function Separator() {
+    return <div className="menu-view__separator"></div>;
+}
+
+export default Separator;
