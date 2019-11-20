d3769f8355a2cb3c68197e7e5d92df807d87d732
Kan Cong <kan.cong@digia.com>
2019-11-20 10:30:26 +0200

    Basic content for main block on menu view

--- src/components/menu/Main.js
+++ src/components/menu/Main.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../../style/modules/menu/_main.scss";
+
 
 
 function Main(props) {
 
     return (
         <div className="menu-view__main">
-            <Link to="/today">Today</Link>
+            <Link to="/today">Lovely Today</Link>
             <Link to="/stats">Stats</Link>
+            <Link to="/tags">Tags</Link>
         </div>
     );
 }
